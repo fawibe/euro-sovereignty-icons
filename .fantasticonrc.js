c0cd879cc81e4892a8356ebd5b729913759ba09e
fawibe <67871961+fawibe@users.noreply.github.com>
2026-01-04 16:06:32 +0100

    Create .fantasticonrc.js

---
+++ .fantasticonrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  inputDir: './icons', // dossier où sont vos SVG
+  outputDir: './dist',
+  fontTypes: ['woff2', 'woff'],
+  assetTypes: ['css', 'json', 'html'],
+  // Si vous utilisez un template personnalisé :
+  templates: {
+    css: './path/to/your/template.css.hbs'
+  }
+};
